468a39431108ca3c5a1ef40f87bdda40ae772df1
Blagoje Blagojevic <blagojeblagojevic562@gmail.com>
2024-04-15 11:38:23 +0200

    Update load_pieces.h

--- CHESS/load_pieces.h
+++ CHESS/load_pieces.h
@@ -2,7 +2,14 @@
 #define LOAD_PIECES
 #include<stdint.h>
 
-#define NUM_OF_PICES 33
+
+#define ARR_SIZE(X) (sizeof((X)) / sizeof((X[0])))
+
+#define TRUE  1
+#define FALSE 0
+
+int NUM_OF_PICES = 32;
+
 
 typedef enum pieces_name{
 	
@@ -30,12 +37,13 @@ typedef enum pieces_name{
 
 
 typedef struct piece{
+	
 	SDL_Texture *texture;
 	SDL_Rect    position;
 	uint8_t pieces_name;
 	char name[100];       //Name of the pices
-	
-	
+	uint8_t is_piece_selected;	
+	uint8_t is_piece_eaten;
 	
 }Piece;
 
@@ -52,6 +60,8 @@ void load_White_King(SDL_Renderer *renderer, Piece *pieces,size_t num_of_pices,s
 	pieces[num_of_pices].position.x = x * SQUER_SIZE;
 	pieces[num_of_pices].position.y = y * SQUER_SIZE;
 	pieces[num_of_pices].pieces_name = WHITE_KING;
+	pieces[num_of_pices].is_piece_selected = FALSE;
+	pieces[num_of_pices].is_piece_eaten = FALSE;
 	//pieces[num_of_pices].name[] = "white_king";
 	strcpy_s(pieces[num_of_pices].name,100,"white_king");
 	
@@ -69,6 +79,8 @@ void load_Black_King(SDL_Renderer *renderer, Piece *pieces,size_t num_of_pices,s
 	pieces[num_of_pices].position.y = y * SQUER_SIZE;
 	strcpy_s(pieces[num_of_pices].name,100,"black_king");
 	pieces[num_of_pices].pieces_name = BLACK_KING;
+	pieces[num_of_pices].is_piece_selected = FALSE;
+	pieces[num_of_pices].is_piece_eaten = FALSE;
 	//printf("%u",BLACK_KING);
 }
 
@@ -84,6 +96,8 @@ void load_White_Queen(SDL_Renderer *renderer, Piece *pieces,size_t num_of_pices,
 	pieces[num_of_pices].position.y = y * SQUER_SIZE;
 	strcpy_s(pieces[num_of_pices].name,100,"white_queen");
 	pieces[num_of_pices].pieces_name = WHITE_QUEEN;
+	pieces[num_of_pices].is_piece_selected = FALSE;
+	pieces[num_of_pices].is_piece_eaten = FALSE;
 }
 
 
@@ -100,6 +114,8 @@ void load_Black_Queen(SDL_Renderer *renderer, Piece *pieces,size_t num_of_pices,
 	//pieces[num_of_pices].name[] = "black_queen";
 	strcpy_s(pieces[num_of_pices].name,100,"black_queen");
 	pieces[num_of_pices].pieces_name = BLACK_QUEEN;
+	pieces[num_of_pices].is_piece_selected = FALSE;
+	pieces[num_of_pices].is_piece_eaten = FALSE;
 }
 
 void load_White_Rook(SDL_Renderer *renderer, Piece *pieces,size_t num_of_pices,size_t x, size_t y){
@@ -115,6 +131,8 @@ void load_White_Rook(SDL_Renderer *renderer, Piece *pieces,size_t num_of_pices,s
 	//pieces[num_of_pices].name[] = "white_rook";
 	strcpy_s(pieces[num_of_pices].name,100,"white_rook");
 	pieces[num_of_pices].pieces_name = WHITE_ROOK;
+	pieces[num_of_pices].is_piece_selected = FALSE;
+	pieces[num_of_pices].is_piece_eaten = FALSE;
 }
 
 void load_Black_Rook(SDL_Renderer *renderer, Piece *pieces,size_t num_of_pices,size_t x, size_t y){
@@ -130,6 +148,8 @@ void load_Black_Rook(SDL_Renderer *renderer, Piece *pieces,size_t num_of_pices,s
 	//pieces[num_of_pices].name[] = "black_rook";
 	strcpy_s(pieces[num_of_pices].name,100,"black_rook");
 	pieces[num_of_pices].pieces_name = BLACK_ROOK;
+	pieces[num_of_pices].is_piece_selected = FALSE;
+	pieces[num_of_pices].is_piece_eaten = FALSE;
 }
 
 void load_White_Knight(SDL_Renderer *renderer, Piece *pieces,size_t num_of_pices,size_t x, size_t y){
@@ -145,6 +165,8 @@ void load_White_Knight(SDL_Renderer *renderer, Piece *pieces,size_t num_of_pices
 	//pieces[num_of_pices].name[] = "white_knight";
 	strcpy_s(pieces[num_of_pices].name,100,"white_knight");
 	pieces[num_of_pices].pieces_name = WHITE_KNIGHT;
+	pieces[num_of_pices].is_piece_selected = FALSE;
+	pieces[num_of_pices].is_piece_eaten = FALSE;
 }
 
 void load_Black_Knight(SDL_Renderer *renderer, Piece *pieces,size_t num_of_pices,size_t x, size_t y){
@@ -160,6 +182,8 @@ void load_Black_Knight(SDL_Renderer *renderer, Piece *pieces,size_t num_of_pices
 	//pieces[num_of_pices].name[] = "black_knight";
 	strcpy_s(pieces[num_of_pices].name,100,"black_knight");
 	pieces[num_of_pices].pieces_name = BLACK_KNIGHT;
+	pieces[num_of_pices].is_piece_selected = FALSE;
+	pieces[num_of_pices].is_piece_eaten = FALSE;
 }
 
 void load_White_Bishop(SDL_Renderer *renderer, Piece *pieces,size_t num_of_pices,size_t x, size_t y){
@@ -175,7 +199,8 @@ void load_White_Bishop(SDL_Renderer *renderer, Piece *pieces,size_t num_of_pices
 	//pieces[num_of_pices].name[] = "white_bishop";
 	strcpy_s(pieces[num_of_pices].name,100,"white_bishop");
 	pieces[num_of_pices].pieces_name = WHITE_BISHOP;
-	
+	pieces[num_of_pices].is_piece_selected = FALSE;
+	pieces[num_of_pices].is_piece_eaten = FALSE;
 }
 
 void load_Black_Bishop(SDL_Renderer *renderer, Piece *pieces,size_t num_of_pices,size_t x, size_t y){
@@ -191,6 +216,8 @@ void load_Black_Bishop(SDL_Renderer *renderer, Piece *pieces,size_t num_of_pices
 	
 	strcpy_s(pieces[num_of_pices].name,100,"black_bishop");
 	pieces[num_of_pices].pieces_name = BLACK_BISHOP;
+	pieces[num_of_pices].is_piece_selected = FALSE;
+	pieces[num_of_pices].is_piece_eaten = FALSE;
 }
 
 void load_White_Pawn(SDL_Renderer *renderer, Piece *pieces,size_t num_of_pices,size_t x, size_t y){
@@ -206,6 +233,8 @@ void load_White_Pawn(SDL_Renderer *renderer, Piece *pieces,size_t num_of_pices,s
 	//pieces[num_of_pices].name[] = "white_pawn";
 	strcpy_s(pieces[num_of_pices].name,100,"white_pawn");
 	pieces[num_of_pices].pieces_name = WHITE_PAWN;
+	pieces[num_of_pices].is_piece_selected = FALSE;
+	pieces[num_of_pices].is_piece_eaten = FALSE;
 }
 
 void load_Black_Pawn(SDL_Renderer *renderer, Piece *pieces,size_t num_of_pices,size_t x, size_t y){
@@ -221,7 +250,132 @@ void load_Black_Pawn(SDL_Renderer *renderer, Piece *pieces,size_t num_of_pices,s
 	//pieces[num_of_pices].name[] = "black_pawn";
 	strcpy_s(pieces[num_of_pices].name,100,"black_pawn");
 	pieces[num_of_pices].pieces_name = BLACK_PAWN;
+	pieces[num_of_pices].is_piece_selected = FALSE;
+	pieces[num_of_pices].is_piece_eaten = FALSE;
 }
+
+void Fen_Loader(SDL_Renderer *renderer,Piece *pieces) {  //Dodati ostale zajebancije
+
+	//char start_position[] = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR";
+	//char start_position[] = "rnbqkbnr/pppppppp/8/8/4P3/8/PPPP1PPP/RNBQKBNR";
+	char start_position[] = "rnbqkbnr/pp1ppppp/8/2p5/4P3/5N2/PPPP1PPP/RNBQKB1R";
+	size_t x = 0, y = 0, num_of_pices = 0;
+
+
+	for(size_t i = 0; i < ARR_SIZE(start_position); i++) {
+
+
+		//assert(num_of_pices <=  NUM_OF_PICES);
+		//if(x == 8) {
+			//x = 0;
+			//y++;
+			//}
+			printf("num of pices %d\n", (int)num_of_pices);
+
+		switch(start_position[i]) {
+
+			case 'r':
+				load_Black_Rook(renderer,pieces,num_of_pices, x++,y);
+				num_of_pices++;
+				break;
+
+			case 'R':
+				load_White_Rook(renderer,pieces,num_of_pices, x++,y);
+				num_of_pices++;
+				break;
+
+			case 'K':
+				load_White_King(renderer,pieces,num_of_pices, x++,y);
+				num_of_pices++;
+				break;
+			
+			case 'k':
+				load_Black_King(renderer,pieces,num_of_pices, x++,y);
+				num_of_pices++;
+				break;
+				
+			case 'Q':
+				load_White_Queen(renderer,pieces,num_of_pices, x++,y);
+				num_of_pices++;
+				break;
+				
+			case 'q':
+				load_Black_Queen(renderer,pieces,num_of_pices, x++,y);
+				num_of_pices++;
+				break;
+				
+			case 'B':
+				load_White_Bishop(renderer,pieces,num_of_pices, x++,y);
+				num_of_pices++;
+				break;	
+			
+			case 'b':
+				load_Black_Bishop(renderer,pieces,num_of_pices, x++,y);
+				num_of_pices++;
+				break;
+				
+			case 'N':
+				load_White_Knight(renderer,pieces,num_of_pices++, x++,y);
+				break;
+				
+			case 'n':
+				load_Black_Knight(renderer,pieces,num_of_pices, x++,y);
+				num_of_pices++;
+				break;
+				
+			case 'P':
+				load_White_Pawn(renderer,pieces,num_of_pices, x++,y);
+				num_of_pices++;
+				break;
+				
+			case 'p':
+				load_Black_Pawn(renderer,pieces,num_of_pices, x++,y);
+				num_of_pices++;
+				break;
+			
+			case '/':	
+				x = 0;
+				y++;
+				break;
+			
+			case '1':
+				x += 1;
+				break;
+				
+			case '2':
+				x += 2;
+				break;
+				
+			case '3':
+				x += 3;
+				break;
+				
+			case '4':
+				x += 4;
+				break;
+				
+			case '5':
+				x += 5;
+				break;
+				
+			case '6':
+				x += 6;
+				break;
+				
+			case '7':
+				x += 7;
+				break;
+			
+			case '8':
+				x	+= 8;
+				break;		
+			}
+						
+		}
+
+	}
+
+
 
 
 #endif
