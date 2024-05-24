85eb7513393349969867d7b451f0d2f5386682e5
Blagoje Blagojevic <blagojeblagojevic562@gmail.com>
2024-05-24 16:21:07 +0200

    Update generator.h

--- bitboardengine/generator.h
+++ bitboardengine/generator.h
@@ -908,7 +908,7 @@ static inline void generate_posible_moves(Board bo, Moves *m, int is_capture, in
 		}
 
 
-	/* //ORDER OF MOVES FOR SEARCH
+	///* //ORDER OF MOVES FOR SEARCH
 	for(int i = 0; i < m->counter; i++) {
 		//IF IS CAPTURE FIRST
 		if(CAPTURE(m->moves[i])) {
@@ -1190,154 +1190,75 @@ const int piece_value[] =  {100, 300, 300, 500, 1000, 10000,
 #define LOG_EVALUATION 0
 
 #define inf 1000000
-const int pawn_score[64] = 
-{
-    90,  90,  90,  90,  90,  90,  90,  90,
-    30,  30,  30,  40,  40,  30,  30,  30,
-    20,  20,  30,  30,  30,  30,  20,  20,
-    10,  10,  10,  20,  20,  10,  10,  10,
-     5,   5,  10,  20,  20,   5,   5,   5,
-     0,   0,   0,   5,   5,   0,   0,   0,
-     0,   0,   0, -10, -10,   0,   0,   0,
-     0,   0,   0,   0,   0,   0,   0,   0
-};
-
-const int pawn_scoreb[64] = 
-{
-     0,   0,   0,   0,   0,   0,   0,   0,
-     0,   0,   0, -10, -10,   0,   0,   0,
-     5,   5,  10,  20,  20,   5,   5,   5,
-     0,   0,   0,   5,   5,   0,   0,   0,
-     10,  10,  10,  20,  20,  10,  10,  10,
-     20,  20,  30,  30,  30,  30,  20,  20,
-     30,  30,  30,  40,  40,  30,  30,  30,
-		 90,  90,  90,  90,  90,  90,  90,  90,
-};
+int mg_table[6][64] = {{
+      0,   0,   0,   0,   0,   0,  0,   0,
+     98, 134,  61,  95,  68, 126, 34, -11,
+     -6,   7,  26,  31,  65,  56, 25, -20,
+    -14,  13,   6,  21,  23,  12, 17, -23,
+    -27,  -2,  -5,  12,  17,   6, 10, -25,
+    -26,  -4,  -4, -10,   3,   3, 33, -12,
+    -35,  -1, -20, -23, -15,  24, 38, -22,
+      0,   0,   0,   0,   0,   0,  0,   0,
+}
 
 
-
-// knight positional score
-const int knight_score[64] = 
-{
-    -5,   0,   0,   0,   0,   0,   0,  -5,
-    -5,   0,   0,  10,  10,   0,   0,  -5,
-    -5,   5,  20,  20,  20,  20,   5,  -5,
-    -5,  10,  20,  30,  30,  20,  10,  -5,
-    -5,  10,  20,  30,  30,  20,  10,  -5,
-    -5,   5,  20,  10,  10,  20,   5,  -5,
-    -5,   0,   0,   0,   0,   0,   0,  -5,
-    -5, -10,   0,   0,   0,   0, -10,  -5
-};
-
-const int knight_scoreb[64] = 
-{
-  	 -5, -10,   0,   0,   0,   0, -10,  -5,
-		 -5,   0,   0,  10,  10,   0,   0,  -5,
-		 -5,   5,  20,  20,  20,  20,   5,  -5,
-		 -5,  10,  20,  30,  30,  20,  10,  -5,
-		 -5,  10,  20,  30,  30,  20,  10,  -5,
-		 -5,   5,  20,  10,  10,  20,   5,  -5,
-		 -5,   0,   0,   0,   0,   0,   0,  -5,
-		 -5,   0,   0,   0,   0,   0,   0,  -5,
-};
+,{
+    -167, -89, -34, -49,  61, -97, -15, -107,
+     -73, -41,  72,  36,  23,  62,   7,  -17,
+     -47,  60,  37,  65,  84, 129,  73,   44,
+      -9,  17,  19,  53,  37,  69,  18,   22,
+     -13,   4,  16,  13,  28,  19,  21,   -8,
+     -23,  -9,  12,  10,  19,  17,  25,  -16,
+     -29, -53, -12,  -3,  -1,  18, -14,  -19,
+    -105, -21, -58, -33, -17, -28, -19,  -23,
+}
 
 
+,{
+    -29,   4, -82, -37, -25, -42,   7,  -8,
+    -26,  16, -18, -13,  30,  59,  18, -47,
+    -16,  37,  43,  40,  35,  50,  37,  -2,
+     -4,   5,  19,  50,  37,  37,   7,  -2,
+     -6,  13,  13,  26,  34,  12,  10,   4,
+      0,  15,  15,  15,  14,  27,  18,  10,
+      4,  15,  16,   0,   7,  21,  33,   1,
+    -33,  -3, -14, -21, -13, -12, -39, -21,
+}
 
 
+,{
+     32,  42,  32,  51, 63,  9,  31,  43,
+     27,  32,  58,  62, 80, 67,  26,  44,
+     -5,  19,  26,  36, 17, 45,  61,  16,
+    -24, -11,   7,  26, 24, 35,  -8, -20,
+    -36, -26, -12,  -1,  9, -7,   6, -23,
+    -45, -25, -16, -17,  3,  0,  -5, -33,
+    -44, -16, -20,  -9, -1, 11,  -6, -71,
+    -19, -13,   1,  17, 16,  7, -37, -26,
+}
+
+,{
+    -28,   0,  29,  12,  59,  44,  43,  45,
+    -24, -39,  -5,   1, -16,  57,  28,  54,
+    -13, -17,   7,   8,  29,  56,  47,  57,
+    -27, -27, -16, -16,  -1,  17,  -2,   1,
+     -9, -26,  -9, -10,  -2,  -4,   3,  -3,
+    -14,   2, -11,  -2,  -5,   2,  14,   5,
+    -35,  -8,  11,   2,   8,  15,  -3,   1,
+     -1, -18,  -9,  10, -15, -25, -31, -50,
+}
 
 
-
-
-
-// bishop positional score
-const int bishop_score[64] = 
-{
-     0,   0,   0,   0,   0,   0,   0,   0,
-     0,   0,   0,   0,   0,   0,   0,   0,
-     0,  20,   0,  10,  10,   0,  20,   0,
-     0,   0,  10,  20,  20,  10,   0,   0,
-     0,   0,  10,  20,  20,  10,   0,   0,
-     0,  10,   0,   0,   0,   0,  10,   0,
-     0,  30,   0,   0,   0,   0,  30,   0,
-     0,   0, -10,   0,   0, -10,   0,   0
-};
-
-const int bishop_scoreb[64] = 
-{
-		 0,   0, -10,   0,   0, -10,   0,   0,
-		 0,  30,   0,   0,   0,   0,  30,   0,
-		 0,  10,   0,   0,   0,   0,  10,   0,
-		 0,   0,  10,  20,  20,  10,   0,   0,
-		 0,   0,  10,  20,  20,  10,   0,   0,
-		 0,  20,   0,  10,  10,   0,  20,   0,
-     0,   0,   0,   0,   0,   0,   0,   0,
-     0,   0,   0,   0,   0,   0,   0,   0,
-     
-     
-     
-     
-     
-     
-};
-
-
-
-
-// rook positional score
-const int rook_score[64] =
-{
-    50,  50,  50,  50,  50,  50,  50,  50,
-    50,  50,  50,  50,  50,  50,  50,  50,
-     0,   0,  10,  20,  20,  10,   0,   0,
-     0,   0,  10,  20,  20,  10,   0,   0,
-     0,   0,  10,  20,  20,  10,   0,   0,
-     0,   0,  10,  20,  20,  10,   0,   0,
-     0,   0,  10,  20,  20,  10,   0,   0,
-     0,   0,   0,  20,  20,   0,   0,   0
-
-};
-
-const int rook_scoreb[64] =
-{
-		0,   0,   0,  20,  20,   0,   0,   0,
-		0,   0,  10,  20,  20,  10,   0,   0,
-		0,   0,  10,  20,  20,  10,   0,   0,
-		0,   0,  10,  20,  20,  10,   0,   0,
-		0,   0,  10,  20,  20,  10,   0,   0,
-		0,   0,  10,  20,  20,  10,   0,   0,
-    50,  50,  50,  50,  50,  50,  50,  50,
-    50,  50,  50,  50,  50,  50,  50,  50,
-};
-
-
-
-// king positional score
-const int king_score[64] = 
-{
--30,-40,-40,-50,-50,-40,-40,-30,
--30,-40,-40,-50,-50,-40,-40,-30,
--30,-40,-40,-50,-50,-40,-40,-30,
--30,-40,-40,-50,-50,-40,-40,-30,
--20,-30,-30,-40,-40,-30,-30,-20,
--10,-20,-20,-20,-20,-20,-20,-10,
- 20, 20,  0,  0,  0,  0, 20, 20,
- 20, 30, 10,  0,  0, 10, 30, 20
-};
-
-const int king_scoreb[64] = 
-{
-20, 30, 10,  0,  0, 10, 30, 20,
-20, 20,  0,  0,  0,  0, 20, 20,
--10,-20,-20,-20,-20,-20,-20,-10,
--20,-30,-30,-40,-40,-30,-30,-20,
--30,-40,-40,-50,-50,-40,-40,-30,
--30,-40,-40,-50,-50,-40,-40,-30,
--30,-40,-40,-50,-50,-40,-40,-30,
--30,-40,-40,-50,-50,-40,-40,-30,
-     
-};
-
-
+,{
+    -65,  23,  16, -15, -56, -34,   2,  13,
+     29,  -1, -20,  -7,  -8,  -4, -38, -29,
+     -9,  24,   2, -16, -20,   6,  22, -22,
+    -17, -20, -12, -27, -30, -25, -14, -36,
+    -49,  -1, -27, -39, -46, -44, -33, -51,
+    -14, -14, -22, -46, -44, -30, -15, -27,
+      1,   7,  -8, -64, -43, -16,   9,   8,
+    -15,  36,  12, -54,   8, -28,  24,  14,
+}};
 
 
 
@@ -1356,19 +1277,27 @@ static inline int evaluation(Board board) {
 	for(int i = p; i <= k; i++) {
 		score -= (NUM(board.piece[i]) * piece_value[i] );
 		}
+	
 
-//	if(board.piece[K] == 0)
-	//	score = -100000;
-	//if(board.piece[k] == 0)
-	//	score = 100000;
-
-
+	 for(int i = P; i <= k; i++){
+		U64 wp = board.piece[i];
+	 while(wp){
+	 	score+=mg_table[i%6][LSB(wp)];
+	 	
+	 	POP(wp,LSB(wp));
+	 }
+	 }
+	 
+	 
+	 
+	 
+	 
 	// WE WILL TAKE INTO  A CONSIDORATITION THAT WHITE WANTS TO MAX SCORE AND BLACK TO MIN
 #if LOG_EVALUATION
 	printf("Board Evaluation %d\n\n",score);
 	//system("pause");
 #endif
-	int v = rand()%100 - 50;
+	//int v = rand()%100 - 50;
 	//printf("v %f", v);
 	//system("pause");
 	//printf("score %f\n", score);
@@ -1472,6 +1401,13 @@ static inline int return_score(Hashmap m, Board b, int *is_store) {
 	return m.score[a];
 	}
 static inline int QuiescansearchT(Board b, Hashmap ma, int alpha, int beta ) {
+	
+	int *is;
+	//Hashmap m, Board b, int *is_store
+	int score = return_score(ma, b, &is);
+	if(is == 1)
+		return score;
+	
 
 	
 	Moves m;
@@ -1580,9 +1516,10 @@ static inline int  searchT(Board board,Hashmap ma, int depth,int alfa, int beta)
 
 		if( depth == 0 ) {
 			
-			//int score_end = QuiescansearchT(board, ma, alfa, beta);
+			int score_end = QuiescansearchT(board, ma, alfa, beta);
 			//int score_end = evaluation(board);
-			int score_end = searchT_capture(board, ma, 7,  alfa, beta);
+			//int score_end = searchT_capture(board, ma, 7,  alfa, beta);
+			
 			store_position(ma, board, score_end);
 			return score_end;
 			}
@@ -1596,7 +1533,7 @@ static inline int  searchT(Board board,Hashmap ma, int depth,int alfa, int beta)
 			//store_position(ma, board, score);
 			memcpy(&board, &temp,sizeof(Board));
 			if( score >= beta ){
-				
+				store_position(ma, board, score);
 				return beta;   //  fail hard beta-cutoff
 			}
 				
@@ -1604,7 +1541,7 @@ static inline int  searchT(Board board,Hashmap ma, int depth,int alfa, int beta)
 			
 				alfa = score; // alpha acts like max in MiniMax
 			}
-		//store_position(ma, board, alfa);
+		store_position(ma, board, alfa);
 			
 		return alfa;
 		}
@@ -1615,23 +1552,29 @@ static inline int  searchT(Board board,Hashmap ma, int depth,int alfa, int beta)
 
 static inline int index_bestT(Board board, Hashmap ma, Moves m) {
 	int min_eval = inf;
+	if(board.side == white)
+		min_eval = -inf;
 	int index = 0;
 	Board temp;
 	int score = 0;
 	memcpy(&temp, &board, sizeof(Board));
 	for(int i = 0; i < m.counter; i++){
 			make_move(&board,m.moves[i]);
-		  int score = searchT(board, ma, 5,  -inf, inf);
+		  int score = searchT(board, ma, 4,  -inf , inf);
 			memcpy(&board,&temp,sizeof(Board));
-			//printf("score : %d\n",score);
-			if(score < min_eval){
+			printf("score : %d\n",score);
+			if(score < min_eval && (board.side == black)){
 				
 				min_eval = score ;
 				index = i;
 				
 			}
-		
-	}
+			
+			if(score > min_eval && (board.side == white)){
+									min_eval = score ;
+									index = i;				
+			}
+		}
 	if(board.side == white)
 		printf("White evaluation %d\n", min_eval);
 		
@@ -1639,4 +1582,3 @@ static inline int index_bestT(Board board, Hashmap ma, Moves m) {
 		printf("Black evaluation %d\n", min_eval);
 	return index;
 }
-
